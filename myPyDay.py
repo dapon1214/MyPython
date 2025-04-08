c054e12cf841f95f08f793a4f9de279e3b4cfbed
Dapon <dapon1214@outlook.com>
2025-04-08 10:23:04 +0800

    太久沒用，後來再測試。

--- myPyDay.py
+++ myPyDay.py
@@ -1,6 +1,16 @@
 import pandas as pd
 
-version = pd.__version__
+# version = pd.__version__
+# print(version)
 
-print(version)
-# 測試僅儲存，但暫時不要commit
+# 顯示所有欄位，不然預設只有幾個欄位
+pd.set_option("display.max_columns", None)
+# 顯示所有row
+pd.set_option("display.max_rows", None)
+
+titanic = pd.read_csv("data/titanic.csv")
+# print(titanic.head())
+
+ages = titanic['Age']
+print(ages)
+
